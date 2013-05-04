e5b985c847b5d1dad86b5bbcf21ee307585bbf98
Romy Irawaty <romi_dj98@Romys-MacBook-Pro.local>
2013-05-04 12:24:13 +0800

    Initial Commit

---
+++ GardenRushV1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GardenRushV1
+//
+//  Created by Romy Irawaty on 4/5/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
